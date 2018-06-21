baf24e886ecf344ec67facb2856075bff79dfd09
Jay Elbourne <jayelbourne@gmail.com>
2018-06-21 00:05:02 -0400

    Tutorial: Input Controller

---
+++ Assets/InputController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        if (hor != 0 || ver != 0)
+            Debug.Log(hor + " " + ver);
+
+        if (Input.GetButtonDown("Jump"))
+            Debug.Log("Jumping");
+
+    }
+}
