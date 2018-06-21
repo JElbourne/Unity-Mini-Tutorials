ad2e59bcd7b021c9caad17d671edf4e6b17ba7fb
Jay Elbourne <jayelbourne@gmail.com>
2018-06-21 00:11:01 -0400

    Put tutorials into folders

---
+++ Assets/Tutorial-Input/InputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class InputController : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            float hor = Input.GetAxis("Horizontal");
+            float ver = Input.GetAxis("Vertical");
+            if (hor != 0 || ver != 0)
+                Debug.Log(hor + " " + ver);
+
+            if (Input.GetButtonDown("Jump"))
+                Debug.Log("Jumping");
+
+        }
+    }
+}
+
